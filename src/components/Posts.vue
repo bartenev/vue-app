a57e9d46b359b4d7271232e96fffb2f8efda068b
Илья <bartenev.ilya@gmail.com>
2021-12-22 12:10:31 +0300

    Добавляет компонент Posts

---
+++ src/components/Posts.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <h2>Posts</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  width: 900px;
+  margin: auto;
+}
+</style>
