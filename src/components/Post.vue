43a4a0567dd007ffbf5d238d3a2a08a6b52f610a
Илья <bartenev.ilya@gmail.com>
2021-12-22 13:31:16 +0300

    Добавляет компонент Post и отрисовывает посты

---
+++ src/components/Post.vue
@@ -0,0 +1,40 @@
+<template>
+  <article class="post">
+    <h2 class="post__title">{{data.title}}</h2>
+    <p class="post__description">{{data.description}}</p>
+    <span class="post__id">{{data.id}}</span>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'Post',
+  props: {
+    data: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.post {
+  width: 250px;
+  height: 200px;
+  border: 1px solid grey;
+  box-sizing: border-box;
+}
+
+.post__title {
+
+}
+
+.post__description {
+  text-overflow: ellipsis;
+}
+
+.post__id {
+
+}
+
+</style>
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,19 +1,64 @@
 <template>
-  <div class="container">
+  <div class="posts">
     <h2>Posts</h2>
+    <ul class="posts__list">
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+      <li class="posts__item">
+        <Post :data="{title: 'title', description: 'description', id: 4}"/>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Post from "./Post";
 export default {
   name: 'Posts',
+  components: {Post},
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.container {
+.posts {
   width: 900px;
   margin: auto;
+}
+.posts__list {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0;
+  padding: 0;
+}
+.posts__item {
+  list-style: none;
+  margin-right: 75px;
+  margin-bottom: 75px;
+}
+.posts__item:nth-child(3n) {
+  margin-right: 0;
 }
 </style>
